d6753f28b0e383b1e9b69cf1a756c118c8cf23ae
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-03-14 15:45:39 +0100

    create product page

---
+++ resources/views/worker/product.blade.php
@@ -0,0 +1,80 @@
+<x-worker.app>
+    <section class="">
+        <h2 class="" aria-level="2" role="heading">
+            SPAX visses 4,5x70mm – 500 pièces
+        </h2>
+        <div class="">
+            <div class="">
+                <img src="{!! asset('assets/img/spax.jpg') !!}" alt="" width="1200" height="1200"
+                       class="">
+
+                <!-- BUTTON TO ADD TO CART -->
+
+                <form action="" method="POST" class="form m-b-128" >
+                    <fieldset>
+
+                        <div class="">
+
+                            <div class="">
+
+                                <button class="">
+                                <span class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                                        <path fill="rgb(0, 0, 0)"
+                                              d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/>
+                                    </svg>
+                                </span>
+                                </button>
+
+                                <input class="" id="paintball-gants" type="number" max=""
+                                       name="supplement" value="0">
+
+
+                                <button class="">
+                                <span class="">
+
+                                    <svg fill="000" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                                        <path
+                                            d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/>
+                                    </svg>
+
+
+                                </span>
+                                </button>
+
+                            </div>
+                        </div>
+
+                    </fieldset>
+                    <button type="submit"
+                            class="">
+                        Mettre dans le panier
+                    </button>
+                </form>
+            </div>
+
+            <dl>
+                <x-worker.definitionterm>
+                    Description
+                </x-worker.definitionterm>
+                <x-worker.definition>
+                    description description descriptiondescription v vvdescriptiondescriptionv descriptionv
+                </x-worker.definition>
+
+                <x-worker.definitionterm>
+                    Notes
+                </x-worker.definitionterm>
+                <x-worker.definition>
+                    description description descriptiondescription v vvdescriptiondescriptionv descriptionv
+                </x-worker.definition>
+
+                <x-worker.definitionterm>
+                    Stock restant
+                </x-worker.definitionterm>
+                <x-worker.definition>
+                    3
+                </x-worker.definition>
+            </dl>
+        </div>
+</section>
+</x-worker.app>
