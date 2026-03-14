82a4385510f4e322e9959648ff83d8c0cc113b4e
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-03-14 15:34:28 +0100

    create test for profile page

---
+++ tests/Feature/Views/ProfileTest.php
@@ -0,0 +1,18 @@
+<?php
+
+test('the application returns a successful response', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+});
+
+
+it('verifies that the profile page is showing it’s contents', function () {
+
+
+    $response = $this->get(route('worker.profile', ['locale' => __('general.currentLocale')]));
+
+    $response->assertStatus(200)
+        ->assertSeeInOrder(["Profil", "Déconnecter", "Changer la langue"]);
+
+});
