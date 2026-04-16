c4f3a59165071c6311dc07c86b557ac3005e3de9
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-16 17:38:42 +0200

    add components for contacts pages

---
+++ resources/views/components/admin/components/admin-primary-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{$href}}" title="{{$title}}" hreflang="{{ app()->getLocale()  }}" class="text-white border-radius-16 admin-primary-button bold t-a-center {{$class}}">
+    {!! $slot !!}
+</a>
