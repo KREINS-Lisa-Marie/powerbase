bc481bfcbcb8ce9753c63e4eed4881668589daa3
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-18 12:47:24 +0200

    create orders edit page

---
+++ resources/views/pages/orders/⚡edit/edit.blade.php
@@ -0,0 +1,98 @@
+@php
+    $orders_state_options = [
+        [
+            'name' => 'En attente',
+        'value' => 'waiting',
+        ],
+        [
+            'name' => 'Complèt',
+            'value' =>'completed',
+        ],
+];
+@endphp
+
+
+
+<main class="admin project-show" id="content">
+    {{--    <x-admin.page-bar>
+            Thomas Fortin   --}}{{--{!! $volunteer->first_name !!}  {!! $volunteer->last_name !!}--}}{{--
+        </x-admin.page-bar>--}}
+    <x-admin.page-bar>
+        {{__('admin/orders.modify_order')}}
+    </x-admin.page-bar>
+    <div class="main-container">
+        <form wire:submit.prevent="store" class="profile-form volunteers-edit">
+            @csrf
+            <fieldset class="project-information max-w-admin-web big-section">
+                <x-admin.components.subtitle>
+                    {{__('admin/orders.general_information')}}
+                </x-admin.components.subtitle>
+                <div class="contact-information-list">
+                    <div>
+                        <div>
+                            <x-admin.components.fields.text name="first_name" value="" placeholder="John" wire=""
+                                                            id="first_name">
+                                {{__('admin/orders.for_who')}}
+                            </x-admin.components.fields.text>
+                        </div>
+                        <div>
+                            <x-admin.components.fields.phone wire="" name="general_phone" id="general_phone" value=""
+                                                             placeholder="048383903">
+                                {{__('admin/orders.phone_person_order')}}
+                            </x-admin.components.fields.phone>
+                        </div>
+
+
+                    </div>
+
+                    <div>
+                        <div>
+                            <x-admin.components.fields.text name="last_name" value="" placeholder="John" wire=""
+                                                            id="last_name">
+                                {{__('admin/orders.project_name')}}
+                            </x-admin.components.fields.text>
+                        </div>
+                        <div>
+                            <x-admin.components.fields.select select_name="project_type"
+                                                              label="{{__('admin/orders.order_state')}}"
+                                                              :options="$orders_state_options" wire="project_type">
+                            </x-admin.components.fields.select>
+                        </div>
+                    </div>
+                </div>
+            </fieldset>
+
+            <div class="split-row">
+                <fieldset class="small-section">
+                    <x-admin.components.subtitle>
+                        {{__('admin/orders.products_used')}}
+                    </x-admin.components.subtitle>
+                    <div>
+                        <x-admin.components.fields.select select_name="vehicle_type"
+                                                          label="{{__('admin/contacts.vehicle_type')}}"
+                                                          wire="vehicle_type">
+                        </x-admin.components.fields.select>
+                    </div>
+                    <div>
+                        <x-admin.components.fields.select select_name="vehicle_type"
+                                                          label="{{__('admin/contacts.vehicle_type')}}"
+                                                          wire="vehicle_type">
+                        </x-admin.components.fields.select>
+                    </div>
+                    <div>
+                        <x-admin.components.fields.select select_name="vehicle_type"
+                                                          label="{{__('admin/contacts.vehicle_type')}}"
+                                                          wire="vehicle_type">
+                        </x-admin.components.fields.select>
+                    </div>
+
+                </fieldset>
+                <div class="admin-information-buttons">
+                    <x-admin.components.admin-primary-button href="" title="" href="" class="">
+                        {{__('admin/orders.save')}}
+                    </x-admin.components.admin-primary-button>
+                </div>
+            </div>
+        </form>
+    </div>
+</main>
