fa697ac59630b1fcb1f9675cb4eaf9ae8623d23d
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-18 13:19:01 +0200

    add translations for sidebar

---
+++ lang/fr/admin/sidebar.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'powerbase' => 'Powerbase',
+    'main_navigation' => 'Navigation principale',
+    'go_to_main_content' => 'Aller au contenu principal',
+    'dashboard' => 'Dashboard',
+    'orders' => 'Commandes',
+    'contacts' => 'Contacts',
+    'products' => 'Products',
+    'projects' => 'Projects',
+    'profile' => 'Profil',
+    'logout' => 'Déconnexion',
+
+    'go_to_the_page_profile' => 'Aller vers la page Profil',
+    'go_to_the_page_orders' => 'Aller vers la page Commandes',
+    'go_to_the_page_contacts' => 'Aller vers la page Contacts',
+    'go_to_the_page_projects' => 'Aller vers la page Projets',
+    'go_to_the_page_products' => 'Aller vers la page Produits',
+    'go_to_the_page_dashboard' => 'Aller vers la page Dashboard',
+
+    'logo' => 'Logo des Powerbase — Aller vers le Dashboard',
+    'go_to_homepage' => 'Aller vers la page Dashboard',
+];
