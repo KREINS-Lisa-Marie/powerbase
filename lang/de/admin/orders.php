481973d1bb479b852fccbe2c08f64e1b54b41e95
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-18 12:50:32 +0200

    create admin orders translations

---
+++ lang/de/admin/orders.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'create_an_order' => 'Eine Bestellung erstellen',
+    'ordered_by' => 'Bestellt von',
+    'product_quantity' => 'Anzahl der Produkte',
+    'ordered_at' => 'Bestellt am',
+    'state' => 'Status',
+
+    'order_number' => 'Bestellnummer',
+    'for_who' => 'Für wen',
+    'phone_person_order' => 'Telefonnummer der Person',
+    'project_name' => 'Projektname',
+    'order_state' => 'Bestellstatus',
+    'ordered_products' => 'Bestellte Produkte',
+    'product_name' => 'Produktname',
+    'quantity' => 'Anzahl der Produkte',
+    'modify_order' => 'Daten bearbeiten',
+    'delete_order' => 'Bestellung löschen',
+    'print_order' => 'Bestellung drucken',
+
+    'general_information' => 'Allgemeine Informationen',
+    'products_used' => 'Bestellte Produkte',
+    'save' => 'Speichern',
+
+    'create_order' => 'Bestellung erstellen',
+    'products_to_order' => 'Zu bestellende Produkte',
+];
---
+++ lang/en/admin/orders.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'create_an_order' => 'Create an order',
+    'ordered_by' => 'Ordered by',
+    'product_quantity' => 'Number of products',
+    'ordered_at' => 'Ordered at',
+    'state' => 'Status',
+
+    'order_number' => 'Order number',
+    'for_who' => 'For whom',
+    'phone_person_order' => 'Phone number of the person',
+    'project_name' => 'Project name',
+    'order_state' => 'Order status',
+    'ordered_products' => 'Ordered products',
+    'product_name' => 'Product name',
+    'quantity' => 'Number of products',
+    'modify_order' => 'Modify data',
+    'delete_order' => 'Delete the order',
+    'print_order' => 'Print the order',
+
+    'general_information' => 'General information',
+    'products_used' => 'Ordered products',
+    'save' => 'Save',
+
+    'create_order' => 'Create the order',
+    'products_to_order' => 'Products to order',
+
+];
