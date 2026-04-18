bc08d26aaf5086f8943ee3acec0605d79ee726e6
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-18 12:24:06 +0200

    adding translations for products and projects

---
+++ lang/de/admin/products.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'general_information' => 'Allgemeine Informationen',
+    'product_name' => 'Produktname',
+    'description' => 'Beschreibung',
+    'notes' => 'Notizen',
+    'stock_number' => 'Lagerbestand',
+    'image' => 'Bild',
+    'modify_product' => 'Produkt bearbeiten',
+    'delete_product' => 'Produkt löschen',
+    'create_product' => 'Produkt erstellen',
+    'create_a_product' => 'Ein Produkt erstellen',
+    'product_image' => 'Bild',
+    'edit_product' => 'Speichern',
+    'updated_at' => 'Aktualisiert am',
+    'in_stock_since' => 'Im Lager seit',
+];
---
+++ lang/en/admin/products.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'general_information' => 'General information',
+    'product_name' => 'Product Name',
+    'description' => 'Description',
+    'notes' => 'Notes',
+    'stock_number' => 'Quantity in inventory',
+    'image' => 'Image',
+    'modify_product' => 'Modify Product',
+    'delete_product' => 'Delete Product',
+    'create_product' => 'Create the product',
+    'create_a_product' => 'Create a product',
+    'product_image' => 'Image',
+    'edit_product' => 'Save',
+    'updated_at' => 'Updated at',
+    'in_stock_since' => 'In Inventory since',
+];
