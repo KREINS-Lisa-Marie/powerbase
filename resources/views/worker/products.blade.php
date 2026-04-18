982eb957d1721536c1ce26ff9088df0c7bd3f29d
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-18 13:23:59 +0200

    fix input

--- resources/views/worker/products.blade.php
+++ resources/views/worker/products.blade.php
@@ -1,14 +1,19 @@
 <x-worker.app>
-    <section>
+    <section class="products-page-section">
         <x-worker.title>
             {{__('worker/products.products')}}
         </x-worker.title>
 
         <label for="search"></label>
-        <input type="search" name="" id="">
+        <input type="search" name="" id="" placeholder="Vis de blablabla">
 
-        @for($i<=8; $i= 0; $i++)
-            <x-worker.product-card>
-        @endfor
+        <ul class="d-flex flex-wrap flex-gap-24">
+            @for( $i= 0; $i<=15;$i++)
+                <li>
+                    <x-worker.product-card productname="Vis 1000"/>
+                </li>
+            @endfor
+        </ul>
+
     </section>
 </x-worker.app>
