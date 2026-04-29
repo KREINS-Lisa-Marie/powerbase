60fa3aa2cc0be4f4bd1a5fbea3230d6014bf64dd
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-29 22:12:23 +0200

    adding data to show page

--- resources/views/pages/projects/⚡show/show.php
+++ resources/views/pages/projects/⚡show/show.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Models\Project;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $project_id;
+
+    public function mount(Project $project)         //avant de render ( 1x seulement)
+    {
+
+        $this->project_id = $project->id;
+    }
+
+    public function render()        //à chaque fois que qqch sur la page change
+    {
+        return view('pages.projects.⚡show.show');
+    }
+
+    public function destroy()
+    {
+        $project = Project::findOrFail($this->project_id);
+        $project->delete();
+        return redirect(route('pages::projects.index', ['locale' => app()->getLocale()]));
+    }
 };
