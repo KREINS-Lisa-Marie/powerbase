abd4dd73dd13185366cb263f1d7036ece4ccf69e
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-04-29 17:32:37 +0200

    creating edit page

---
+++ resources/views/pages/profile/⚡edit/edit.blade.php
@@ -0,0 +1,38 @@
+<main class="admin profile-edit" id="content">
+    <x-admin.page-bar>
+        {{__('admin/profile.change_my_password')}}
+    </x-admin.page-bar>
+
+    <div class="main-container">
+        <section class="admin-profile">
+            <h2 class="sro">
+                {{__('admin/profile.profile')}}
+            </h2>
+            <form wire:submit.prevent="save"
+                  class="profile-form split-row ">
+                @csrf
+                <fieldset class="contact-information max-w-admin-web small-section">
+                <div class="profile-inputs">
+                    <x-auth.form.password wire="password" name="user_password" id="user_password" value=""
+                                          placeholder="Entrez un mot de passe">
+                        {{__('admin/profile.user_password')}}
+                    </x-auth.form.password>
+                </div>
+
+                <div class="profile-inputs">
+                    <x-auth.form.password-confirmation wire="password_confirmation" name="user_password_confirmation" id="user_password_confirmation" value=""
+                                                       placeholder="Retapez votre mot de passe">
+                        {{__('admin/profile.user_password_confirmation')}}
+                    </x-auth.form.password-confirmation>
+                </div>
+                </fieldset>
+                    <div class="admin-information-buttons">
+                        <x-admin.components.form-button>
+                            {{__('admin/profile.save')}}
+                        </x-admin.components.form-button>
+                    </div>
+            </form>
+
+        </section>
+    </div>
+</main>
