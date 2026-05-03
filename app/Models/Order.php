a318a9a08fc673b605d062cb90d98d48082ea27a
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:08:57 +0200

    create order model

---
+++ app/Models/Order.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['for_who','phone', 'project_name', 'order_state', 'ordered_at'];
+
+    public function products():BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function projects():BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function users():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
