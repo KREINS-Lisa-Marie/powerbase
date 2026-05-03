590d0cef059afc831ed26ce622a2c922d731e734
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:10:13 +0200

    create orders test for index page

--- resources/views/pages/orders/⚡index/index.test.php
+++ resources/views/pages/orders/⚡index/index.test.php
@@ -1,6 +1,17 @@
 <?php
 
+use App\Models\Project;
+use App\Models\User;
 use Livewire\Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\actingAs;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function(){
+    $this-> user = User::factory()-> create();
+    \Pest\Laravel\actingAs($this-> user);});
+
 
 it('renders successfully', function () {
     Livewire::test('pages::orders.index')
@@ -13,3 +24,28 @@ it('verifies that the orders index page is showing content elements in the right
         ->assertStatus(200)
         ->assertSee(['Créer une commande', 'Trier', 'Rechercher', 'Commandé par', 'Nombre', 'Commandé le', 'Statut']);
 });
+
+
+
+it('displays a list of orders on the orders index page', function () {
+
+    //Event::fake();
+
+    $user = User::factory()->create();
+    $project = Project::factory()->create();
+
+    $orders = \App\Models\Order::factory(3)->create([
+        'for_who'=>$user->id,
+        'project_name'=>$project->project_name
+    ]);
+
+    Livewire::test('pages::orders.index')
+        ->assertSee($orders[0]->id)
+        ->assertSee($orders[1]->id)
+        ->assertSee($orders[2]->id);
+
+    /*    \Pest\Laravel\assertDatabaseHas('projects',
+        ['name'=>'Projet Client',
+        ]);*/
+}
+);
