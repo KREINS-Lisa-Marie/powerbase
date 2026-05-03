9c5ad97773eefbc28ff82224526593c646046111
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:10:57 +0200

    add data and translations to index page

--- resources/views/pages/orders/⚡index/index.blade.php
+++ resources/views/pages/orders/⚡index/index.blade.php
@@ -2,17 +2,17 @@
     $filter_options =[
            [
             'name' => 'ABC',
-        'value' =>'ABC',
+        'value' =>'abc',
         ],
                    [
             'name' => 'ZYX',
-        'value' =>'ZYX',
+        'value' =>'zyx',
         ],
                    [
-            'name' => 'latest',
-        'value' =>'plus récents',
+            'name' => __('admin/orders.latest'),
+        'value' =>'most recent',
         ],
-    ]
+    ];
 @endphp
 <main class="admin orders-index-page" id="content">
     <x-admin.page-bar>
@@ -22,8 +22,8 @@
         <div class="admin-filters-buttons max-w-admin-web">
             <div class="top-row">
                 <x-admin.components.admin-primary-button
-                    href="{{route('pages::contacts.create', ['locale' => __('general.currentLocale')])}}"
-                    title="Aller sur la page 'Créer une commande'" class="">
+                    href="{{route('pages::orders.create', ['locale' => __('general.currentLocale')])}}"
+                    title="{{__('admin/orders.go_to_create_order')}}" class="">
                     {{__('admin/orders.create_an_order')}}
                 </x-admin.components.admin-primary-button>
             </div>
@@ -36,7 +36,7 @@
 
         <section class="orders-list">
             <h2 class="sro">
-
+                {{__('admin/orders.orders_list')}}
             </h2>
             <table class="table max-w-admin-web">
                 <thead>
@@ -57,38 +57,48 @@
                 </thead>
                 <tbody>
 
-                @for($i=0; $i<10; $i++)
-                    {{--@foreach($this->searchedUsers() as $volunteer)--}}
+                    @forelse($orders as $order)
+                        @php
+                            $user = \App\Models\User::findOrFail($order->for_who)
+                        @endphp
                     <tr class="table-row position-relative">
                         <x-admin.components.table.table-td class="table-full_name">
                             <span class="show-web">{{__('admin/orders.ordered_by')}}</span>
-                            Black Jack
-                            {{--<img src="{!! asset('assets/img/border-collie.jpg') !!}" alt="image du chien" class="border-r-big">--}}
+                            {{$user->first_name}} {{$user->last_name}}
                         </x-admin.components.table.table-td>
                         <x-admin.components.table.table-td class="table-name fw-medium">
                             <span
                                 class="show-web">{{__('admin/orders.product_quantity')}}</span>{{--{!! $volunteer->last_name !!} {!! $volunteer->first_name !!}--}}
                             john@doe.com
+
                         </x-admin.components.table.table-td>
                         <x-admin.components.table.table-td class="table-state">
                             <span
-                                class="show-web">{{__('admin/orders.ordered_at')}}</span>{{--{!! $volunteer->phone !!}--}}
-                            2943473207
+                                class="show-web">{{__('admin/orders.ordered_at')}}</span>
+                            {{ date('d/m/Y', strtotime($order->ordered_at)) }}
                         </x-admin.components.table.table-td>
                         <x-admin.components.table.table-td class="table-species">
                             <span
-                                class="show-web">{{__('admin/orders.state')}}</span>{{--{!! $volunteer->is_admin?   __('admin/volunteers.admin'): __('admin/volunteers.volunteer') !!}--}}
-                            Electricien
-                            <a href="{{--{{route('pages::volunteers.show',  ['locale' => __('general.currentLocale'),  'volunteer' => $volunteer->id])}}--}}"
-                               title="aller vers la page de la commande" class="card-link">
+                                class="show-web">{{__('admin/orders.state')}}</span>
+                            {{$order->order_state == 'completed' ? __('admin/orders.completed') : __('admin/orders.pending')}}
+                            <a href="{{route('pages::orders.show',  ['locale' => __('general.currentLocale'),  'order' => $order->id])}}"
+                               title="{{__('admin/orders.go_to_order_page')}}" class="card-link">
                             </a>
                         </x-admin.components.table.table-td>
                     </tr>
-                    {{--@endforeach--}}
-                @endfor
+                        @empty
+                            <tr class="table-row position-relative">
+                                <x-admin.components.table.table-td class="table-full_name">
+                                    {{__('admin/projects.no_result')}}
+                                </x-admin.components.table.table-td>
+                            </tr>
+                        @endforelse
                 </tbody>
             </table>
         </section>
+        <div class="pagination max-w-admin-web">
+            {{ $orders->links() }}
+        </div>
     </div>
 
 </main>
--- resources/views/pages/orders/⚡index/index.php
+++ resources/views/pages/orders/⚡index/index.php
@@ -1,8 +1,34 @@
 <?php
 
+use App\Models\Order;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+/*
+    public $orders;
+    public function mount()         //avant de render ( 1x seulement)
+    {
+        $this->orders = Order::get();
+    }
+
+    public function render()        //à chaque fois que qqch sur la page change
+    {
+        return view('pages.orders.⚡index.index');
+    }*/
+
+    public  $search = '';
+
+    public function render()        //à chaque fois que qqch sur la page change
+    {
+        return view('pages.orders.⚡index.index', [
+            'orders' => \App\Models\Order::query()
+                ->where('for_who', 'like', '%' . $this->search . '%')
+                ->orWhere('phone', 'like', '%' . $this->search . '%')
+                ->orWhere('ordered_at', 'like', '%' . $this->search . '%')
+                ->orWhere('order_state', 'like', '%' . $this->search . '%')
+                ->orderBy('for_who', 'asc')
+                ->paginate(10),
+        ]);
+    }
 };
