4e5a0157ffd7341fca07fac3af24aa2907c44a83
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:03:31 +0200

    create test for products show

--- resources/views/pages/products/⚡show/show.test.php
+++ resources/views/pages/products/⚡show/show.test.php
@@ -1,18 +1,55 @@
 <?php
 
+use App\Models\Product;
+use App\Models\User;
 use Livewire\Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\actingAs;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function(){
+    $this-> user = User::factory()-> create();
+    \Pest\Laravel\actingAs($this-> user);});
+
+
 
 it('renders successfully', function () {
-    Livewire::test('pages::products.show')
+
+    $product = Product::factory()->create();
+
+    Livewire::test('pages::products.show',
+        [
+            'product' => $product->id,
+        ])
         ->assertStatus(200);
 });
 
 
-
-
-
 it('verifies that the products show page is showing content elements in the right order', function () {
-    Livewire::test('pages::products.show')
+
+    $product = Product::factory()->create();
+
+    Livewire::test('pages::products.show',
+        [
+            'product' => $product->id,
+        ])
         ->assertStatus(200)
         ->assertSee(['Informations générales', 'Nom du produit', 'img', 'Modifier le produit', 'Supprimer le produit']);
+});
+
+
+it('displays a detail page of a product and verifies if there is data', function () {
+
+    //Event::fake();
+    $product = Product::factory()->create();
+
+    Livewire::test('pages::products.show', [
+        'product' => $product->id,
+    ])
+        ->assertSee($product->product_name)
+        ->assertSee($product->product_description)
+        ->assertSee($product->product_notes)
+        ->assertSee($product->quantity)
+        ->assertSee($product->product_image);
 });
