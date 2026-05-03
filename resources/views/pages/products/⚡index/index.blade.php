11d0f7f1f2322750fa12ceabece1c2a66f41ae49
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:02:39 +0200

    add data to products index

--- resources/views/pages/products/⚡index/index.blade.php
+++ resources/views/pages/products/⚡index/index.blade.php
@@ -2,26 +2,26 @@
     $filter_options =[
            [
             'name' => 'ABC',
-        'value' =>'ABC',
+        'value' =>'abc',
         ],
                    [
             'name' => 'ZYX',
-        'value' =>'ZYX',
+        'value' =>'zyx',
         ],
                    [
-            'name' => 'latest',
-        'value' =>'plus récents',
+            'name' => __('admin/products.most_recent'),
+        'value' =>'latest',
         ],
     ]
 @endphp
 <main class="admin products-index-page" id="content">
     <x-admin.page-bar>
-        Accueil{{--{{__('admin/dashboard.dashboard')}}--}}
+        {{__('admin/products.products')}}
     </x-admin.page-bar>
     <div class="main-container">
         <div class="admin-filters-buttons max-w-admin-web">
             <div class="top-row">
-                <x-admin.components.admin-primary-button href="{{route('pages::products.create', ['locale' => __('general.currentLocale')])}}" title="Aller sur la page 'Créer un produit'" class="">
+                <x-admin.components.admin-primary-button href="{{route('pages::products.create', ['locale' => __('general.currentLocale')])}}" title="{{__('admin/products.got_to_create_product')}}" class="">
                     {{__('admin/products.create_a_product')}}
                 </x-admin.components.admin-primary-button>
             </div>
@@ -32,7 +32,7 @@
         </div>
         <section class="products-list">
             <h2 class="sro">
-
+                {{__('admin/products.products_list')}}
             </h2>
             <table class="table max-w-admin-web">
                 <thead>
@@ -53,31 +53,39 @@
                 </thead>
                 <tbody>
 
-                @for($i=0; $i<10; $i++)
-                    {{--@foreach($this->searchedUsers() as $volunteer)--}}
+                    @forelse($products as $product)
                     <tr class="table-row position-relative">
                         <x-admin.components.table.table-td class="table-full_name">
                             <span class="show-web">{{__('admin/products.product_name')}}</span>
-                            Black Jack
-                            {{--<img src="{!! asset('assets/img/border-collie.jpg') !!}" alt="image du chien" class="border-r-big">--}}
+                            {{$product->product_name}}
                         </x-admin.components.table.table-td>
                         <x-admin.components.table.table-td class="table-name fw-medium">
-                            <span class="show-web">{{__('admin/products.stock_number')}}</span>{{--{!! $volunteer->last_name !!} {!! $volunteer->first_name !!}--}}
-                            john@doe.com
+                            <span class="show-web">{{__('admin/products.stock_number')}}</span>
+                            {{$product->quantity}}
                         </x-admin.components.table.table-td>
                         <x-admin.components.table.table-td class="table-state">
-                            <span class="show-web">{{__('admin/products.in_stock_since')}}</span>{{--{!! $volunteer->phone !!}--}} 2943473207
+                            <span class="show-web">{{__('admin/products.in_stock_since')}}</span>
+                            {{ date('d/m/Y', strtotime($product->created_at)) }}
                         </x-admin.components.table.table-td>
                         <x-admin.components.table.table-td class="table-species">
-                            <span class="show-web">{{__('admin/products.updated_at')}}</span>{{--{!! $volunteer->is_admin?   __('admin/volunteers.admin'): __('admin/volunteers.volunteer') !!}--}}Electricien
-                            <a href="{{--{{route('pages::volunteers.show',  ['locale' => __('general.currentLocale'),  'volunteer' => $volunteer->id])}}--}}" title="aller vers la page de l’animal" class="card-link">
+                            <span class="show-web">{{__('admin/products.updated_at')}}</span>
+                            {{ date('d/m/Y', strtotime($product->updated_at)) }}
+                            <a href="{{route('pages::products.show',  ['locale' => __('general.currentLocale'),  'product' => $product->id])}}" title="{{__('admin/products.got_to_product_page')}}" class="card-link">
                             </a>
                         </x-admin.components.table.table-td>
                     </tr>
-                    {{--@endforeach--}}
-                @endfor
+                    @empty
+                        <tr class="table-row position-relative">
+                            <x-admin.components.table.table-td class="table-full_name">
+                                {{__('admin/products.no_result')}}
+                            </x-admin.components.table.table-td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </section>
+        <div class="pagination max-w-admin-web">
+            {{ $products->links() }}
+        </div>
     </div>
 </main>
