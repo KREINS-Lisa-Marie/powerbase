e61e6c2a6aea394e93ef8bbd06ff795e2fa72ac8
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:11:18 +0200

    create tests for orders show page

--- resources/views/pages/orders/⚡show/show.test.php
+++ resources/views/pages/orders/⚡show/show.test.php
@@ -1,17 +1,96 @@
 <?php
 
+use App\Models\Order;
+use App\Models\Project;
+use App\Models\User;
 use Livewire\Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\actingAs;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function(){
+    $this-> user = User::factory()-> create();
+    \Pest\Laravel\actingAs($this-> user);});
+
 
 it('renders successfully', function () {
-    Livewire::test('pages::orders.show')
+
+    $random_project_state = 'Particulier';
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $project = Project::factory()->create([
+        'person_in_charge' => $worker->id,
+        'phone_in_charge' => $worker->phone,
+        'project_type' => $random_project_state,
+]);
+
+    $order = Order::factory()->create([
+        'for_who'=>$worker->id,
+        'project_name'=>$project->project_name
+    ]);
+
+    Livewire::test('pages::orders.show',
+        [
+            'order' => $order->id,
+        ])
         ->assertStatus(200);
 });
 
 
-
-
 it('verifies that the orders show page is showing content elements in the right order', function () {
-    Livewire::test('pages::orders.show')
+
+    $random_project_state = 'Particulier';
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $project = Project::factory()->create([
+        'person_in_charge' => $worker->id,
+        'phone_in_charge' => $worker->phone,
+        'project_type' => $random_project_state,
+    ]);
+
+    $order = Order::factory()->create([
+        'for_who'=>$worker->id,
+        'project_name'=>$project->project_name,
+    ]);
+
+    Livewire::test('pages::orders.show', [
+        'order' => $order->id,
+    ])
         ->assertStatus(200)
         ->assertSee(['Informations générales', 'Pour qui', 'Nom du projet', 'Produits commandés', 'Modifier', 'Supprimer', 'Imprimer']);
+});
+
+
+it('displays a detail page of a orders and verifies if there is data', function () {
+
+    //Event::fake();
+    $random_project_state = 'Particulier';
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $project = Project::factory()->create([
+        'person_in_charge' => $worker->id,
+        'phone_in_charge' => $worker->phone,
+        'project_type' => $random_project_state,
+    ]);
+    $order = Order::factory()->create(
+        [
+            'for_who'=>$worker->id,
+            'project_name'=>$project->project_name,
+        ]
+    );
+
+    Livewire::test('pages::orders.show', [
+        'order' => $order->id,
+    ])
+        ->assertSee($order->for_who)
+        ->assertSee($worker->phone)
+        ->assertSee($order->project_name)
+        ->assertSee($order->order_state);
 });
