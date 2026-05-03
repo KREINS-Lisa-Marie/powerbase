b4be2e4815c5a865d8e78089b26adb9e81577a48
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-03 11:05:59 +0200

    add translations

--- lang/fr/admin/products.php
+++ lang/fr/admin/products.php
@@ -16,4 +16,16 @@ return [
     'updated_at' => 'Mise à jour',
     'in_stock_since' => 'En stock depuis',
 
+    'modify' => 'Modifier',
+    'products' => 'Produits',
+
+
+    'placeholder_description' => 'Le produit consiste en...',
+    'placeholder_note' => 'Changement de couleur depuis...',
+    'most_recent' => 'Plus récents',
+    'got_to_create_product' => "Aller sur la page 'Créer un produit'",
+    'got_to_product_page' => 'Aller sur la page du produit',
+    'products_list' => 'Liste des produits',
+    'the_product_image' => 'Image du produit',
+
 ];
