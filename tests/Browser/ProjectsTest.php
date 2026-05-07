b7c5cd34bb76e27f21944bd63aa2adab3beaa925
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-07 11:07:50 +0200

    make projects tests

---
+++ tests/Browser/ProjectsTest.php
@@ -0,0 +1,205 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
+
+uses(RefreshDatabase::class);
+
+// Projects
+
+it('can click a project card and go to the show page', function () {
+    //Event::fake();
+
+    $user = User::factory()->create();
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $random_project_state = 'Particulier';
+
+    $project = \App\Models\Project::factory()->create([
+        'person_in_charge' => $user->id,
+        'phone_in_charge' => $user->phone,
+        'project_type' => $random_project_state,
+    ]);
+
+    $locale = app()->getLocale();
+    actingAs($user);
+
+    $route = route('pages::projects.index',['locale' => $locale]);
+
+    visit($route)
+        ->click(".card-link")
+        ->assertUrlIs(route('pages::projects.show', [
+            'project' => $project->id,
+            'locale' => $locale
+        ]));
+});
+
+it('can click on create a project and go to the create page', function () {
+    //Event::fake();
+
+    $user = User::factory()->create();
+    $locale = app()->getLocale();
+    actingAs($user);
+
+    $route = route('pages::projects.index',['locale' => $locale]);
+
+    visit($route)
+        ->click(".admin-primary-button")
+        ->assertUrlIs(route('pages::projects.create', [
+            'locale' => $locale
+        ]));
+});
+
+it('can click the edit button of a project and go to the edit page', function () {
+    //Event::fake();
+
+    $user = User::factory()->create();
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $random_project_state = 'Particulier';
+
+    $project = \App\Models\Project::factory()->create([
+        'person_in_charge' => $user->id,
+        'phone_in_charge' => $user->phone,
+        'project_type' => $random_project_state,
+    ]);
+    $locale = app()->getLocale();
+    actingAs($user);
+
+    $route = route('pages::projects.show',[
+        'locale' => $locale,
+        'project' => $project->id,
+    ]);
+
+    visit($route)
+        ->click(".admin-primary-button")
+        ->assertUrlIs(route('pages::projects.edit', [
+            'project' => $project->id,
+            'locale' => $locale
+        ]));
+});
+
+it('can click on the delete button, delete the project and go back to the index page', function () {
+    //Event::fake();
+
+    $user = User::factory()->create();
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $random_project_state = 'Particulier';
+
+    $project = \App\Models\Project::factory()->create([
+        'person_in_charge' => $user->id,
+        'phone_in_charge' => $user->phone,
+        'project_type' => $random_project_state,
+    ]);
+    $locale = app()->getLocale();
+    actingAs($user);
+
+    $route = route('pages::projects.show',[
+        'locale' => $locale,
+        'project' => $project->id,
+    ]);
+
+    visit($route)
+        ->click("#delete-element")
+        ->assertUrlIs(route('pages::projects.index', [
+            'locale' => $locale
+        ]));
+
+    assertDatabaseMissing('projects', [
+        'id' => $project->id,
+    ]);
+});
+
+
+
+
+it('can create a project and redirect to the show page', function () {
+    //Event::fake();
+
+    $user = User::factory()->create();
+    $locale = app()->getLocale();
+    actingAs($user);
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $random_project_state = 'private';
+
+
+    $route = route('pages::projects.create',['locale' => $locale]);
+
+    visit($route)
+        ->assertSee('Créer un projet')
+        ->fill('project_name', 'Panneaux PV Colignon')
+        ->select('person_in_charge', $worker->id)
+        ->fill('phone_in_charge', $worker->phone)
+        ->select('project_type', $random_project_state)
+        ->fill('client_name', 'Madame Colignon')
+        ->fill('project_address', 'Rue de l’école 2')
+        ->fill('project_description', 'Ajout de panneaux solaires')
+        ->click('Créer le projet')
+        ->assertSee('Panneaux PV Colignon');
+
+    assertDatabaseHas('projects', [
+        'project_name' => 'Panneaux PV Colignon',
+    ]);
+});
+
+it('can edit a project and redirect to the show page', function () {
+    //Event::fake();
+
+    $user = User::factory()->create();
+    $locale = app()->getLocale();
+    actingAs($user);
+
+
+    $worker = User::factory()->create([
+        'job' => 'worker',
+    ]);
+    $random_project_state = 'private';
+
+
+    $project = \App\Models\Project::factory()->create([
+        'person_in_charge' => $user->id,
+        'phone_in_charge' => $user->phone,
+        'project_type' => $random_project_state,
+    ]);
+
+    $route = route('pages::projects.edit',[
+        'locale' => $locale,
+        'project' => $project->id
+    ]);
+
+    $person_in_charge = User::where('id', $project->person_in_charge)->first();
+    $name_person_in_charge = "$person_in_charge->first_name $person_in_charge->last_name";
+    $new_project_state = 'corporate';
+
+    visit($route)
+        ->assertSee('Modifier')
+        ->fill('project_name', $project->project_name)
+        ->select('person_in_charge', $name_person_in_charge)
+        ->fill('phone_in_charge', $project->phone_in_charge)
+        ->fill('project_address', $project->project_address)
+        ->fill('project_description', $project->project_description)
+        ->fill('client_name', 'Monsieur Bonhomme')
+        ->select('project_type', $new_project_state)
+        ->click('Enregistrer')
+        ->assertSee($project->project_name)
+        ->assertUrlIs(route('pages::projects.show', [
+            'locale' => $locale,
+            'project' => $project->id
+        ]));
+
+    assertDatabaseHas('projects', [
+        'client_name' => 'Monsieur Bonhomme',
+    ]);
+});
