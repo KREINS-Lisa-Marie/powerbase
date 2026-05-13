9e74be87da0f9e7c1591bb108d82ae2d1dd4b416
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-13 10:07:22 +0200

    creating order item factory

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderItemFactory extends Factory
+{
+    protected $model = OrderItem::class;
+
+    public function definition(): array
+    {
+
+
+        $product_id =  Product::exists() ? Product::all()->pluck('id')->random() : Product::factory()->create()->id;
+        $random_order = Order::exists() ? Order::all()->pluck('id')->random() : Order::factory()->create()->id;
+        $quantity = rand(0, 150);
+
+
+        return [
+            'order_id' => $random_order,
+            'product_id' => $product_id,
+            'quantity' => $quantity,
+        ];
+    }
+}
