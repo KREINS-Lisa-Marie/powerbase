e1883b688ad98252c32be8e1c4f82b53d49ab76f
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-13 15:24:51 +0200

    adding create tests

--- resources/views/pages/orders/⚡create/create.test.php
+++ resources/views/pages/orders/⚡create/create.test.php
@@ -17,9 +17,47 @@ it('renders successfully', function () {
         ->assertStatus(200);
 });
 
-
 it('verifies that the orders create page is showing content elements in the right order', function () {
     Livewire::test('pages::orders.create')
         ->assertStatus(200)
-        ->assertSee(['Informations générales', 'Pour qui', 'Nom du projet', 'Produits à commander', 'Créer la commande']);
+        ->assertSeeInOrder(['Informations générales', 'Pour qui', 'Nom du projet', 'Produits à commander', 'Créer la commande']);
 });
+
+it('redirects to the orders index route after the successfull creation of an order',
+    function () {
+
+        $user = User::factory()->create();
+        $random_order_state = rand(0, 1) ? \App\Enums\OrderStates::Completed->value : \App\Enums\OrderStates::Pending->value;
+
+        $project = \App\Models\Project::factory()->create([
+            'user_id'=> $user->id,
+        ]);
+
+        Livewire::test('pages::orders.create')
+            ->set('user_id', $user->id)
+            ->set('order_state', $random_order_state)
+            ->set('project_id', $project->id)
+            ->set('ordered_at', now()->format('Y-m-d'))
+            ->call('store')
+            ->assertHasNoErrors()
+            ->assertRedirect(route('pages::orders.index', ['locale' => __('general.currentLocale')]));
+    }
+);
+
+it(
+    'verifies that the orders.create route displays a form to create an order',
+    function (string $locale, string $main_heading, string $button_text) {
+
+        \Illuminate\Support\Facades\App::setLocale($locale);
+
+        Livewire::test('pages::orders.create')
+            ->assertSee("$main_heading", false)
+            ->assertSeeHtml('<form wire:submit.prevent="store"')
+            ->assertSee($button_text, false);
+    }
+)->with(
+    [
+        ['fr', 'Créer une commande', 'Créer la commande'],
+        ['en', 'Create an order', 'Create the order'],
+    ]
+);
