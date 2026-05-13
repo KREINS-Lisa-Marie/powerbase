4c401385c1843ed23a346018133a2afe38fc199b
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-13 11:11:18 +0200

    fix products edit test

--- resources/views/pages/products/⚡edit/edit.test.php
+++ resources/views/pages/products/⚡edit/edit.test.php
@@ -1,14 +1,27 @@
 <?php
 
+use App\Models\User;
 use Livewire\Livewire;
 
+
+beforeEach(function(){
+    $this-> user = User::factory()-> create();
+    \Pest\Laravel\actingAs($this-> user);});
+
+
 it('renders successfully', function () {
-    Livewire::test('pages::products.edit')
+
+    $product = \App\Models\Product::factory()->create();
+
+    Livewire::test('pages::products.edit', ['product'=>$product])
         ->assertStatus(200);
 });
 
 it('verifies that the products edit page is showing content elements in the right order', function () {
-    Livewire::test('pages::products.edit')
+
+    $product = \App\Models\Product::factory()->create();
+
+    Livewire::test('pages::products.edit', ['product'=>$product])
         ->assertStatus(200)
         ->assertSee(['Informations générales', 'Nom du produit', 'Image', 'Enregistrer']);
 });
