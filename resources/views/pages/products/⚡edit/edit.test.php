41a92cc9f2a1672a3105ad3e4bfb797e0b5d5960
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-13 15:24:33 +0200

    adding edit tests

--- resources/views/pages/products/⚡edit/edit.test.php
+++ resources/views/pages/products/⚡edit/edit.test.php
@@ -25,3 +25,55 @@ it('verifies that the products edit page is showing content elements in the righ
         ->assertStatus(200)
         ->assertSee(['Informations générales', 'Nom du produit', 'Image', 'Enregistrer']);
 });
+
+
+it('redirects to the products show route after the successfull edit of a product',
+    function () {
+
+        $user = User::factory()->create();
+
+        $product = \App\Models\Product::factory()->create([
+            'product_name'=>'Bouteille',
+        ]);
+
+        Livewire::test('pages::products.edit', [
+            'product' => $product,
+        ])
+            ->set('product_name', 'verre')
+            ->set('brand', $product->brand)
+            ->set('ref_article', $product->ref_article)
+            ->set('gtin', $product->gtin)
+            ->set('product_description', $product->product_description)
+            ->set('product_notes', $product->product_notes)
+            ->set('product_image', $product->product_image)
+            ->set('quantity', $product->quantity)
+            ->call('save')
+            ->assertHasNoErrors()
+            ->assertRedirect(route('pages::products.show', [
+                'product' => $product->id,
+                'locale' => __('general.currentLocale')]));
+    }
+);
+
+it(
+    'verifies that the products.edit route displays a form to edit a product',
+    function (string $locale, string $main_heading, string $button_text) {
+
+        \Illuminate\Support\Facades\App::setLocale($locale);
+        $user = User::factory()->create();
+
+        $product = \App\Models\Product::factory()->create();
+
+        Livewire::test('pages::products.edit',  [
+            'product' => $product,
+        ])
+            ->assertSee("$main_heading", false)
+            ->assertSeeHtml('<form wire:submit="save"')
+            ->assertSee($button_text, false);
+    }
+)->with(
+    [
+        ['fr', 'Modifier', 'Enregistrer'],
+        ['en', 'Modify', 'Save'],
+    ]
+);
