8348f58a55f1de5fb2e0cca1fdd88ff5d68ee7b2
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-18 10:51:35 +0200

    fix th for sort

--- resources/views/components/admin/components/table/table-th.blade.php
+++ resources/views/components/admin/components/table/table-th.blade.php
@@ -1,3 +1,10 @@
-<th scope="{{ $scope }}" class="bold">
+<th scope="{{ $scope }}" class="bold" {{ $attributes }}>
     {!! $slot!!}
+    @if($direction === 'desc')
+        ▲
+    @elseif($direction === 'asc')
+        ▼
+    @elseif($sortable)
+        ▼▲
+    @endif
 </th>
