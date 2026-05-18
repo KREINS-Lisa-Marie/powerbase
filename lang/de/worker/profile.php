031abf1b9b7ec6d7d12c5f6f438cabff982138c2
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-18 16:17:51 +0200

    make translations

--- lang/de/worker/profile.php
+++ lang/de/worker/profile.php
@@ -3,5 +3,12 @@
 return [
     'profile' => 'Profil',
     'logout' => 'Abmelden',
-    'change_language' => 'Sprache wechseln — ',
+    'change_language' => 'Sprache wechseln',
+
+    'change_language_to_french' => "Sprache zu 'Französisch' ändern",
+    'change_language_to_german' => "Sprache zu 'Deutsch' ändern",
+    'change_language_to_english' => "Sprache zu 'Englisch' ändern",
+    'french' => "Französisch",
+    'german' => "Deutsch",
+    'english' => "Englisch",
 ];
--- lang/fr/worker/profile.php
+++ lang/fr/worker/profile.php
@@ -3,5 +3,12 @@
 return [
     'profile' => 'Profil',
     'logout' => 'Déconnecter',
-    'change_language' => 'Changer la langue — ',
+    'change_language' => 'Changer la langue',
+
+    'change_language_to_french'=>"Changer la langue vers 'Français'",
+    'change_language_to_german'=>"Changer la langue vers 'Allemand'",
+    'change_language_to_english'=>"Changer la langue vers 'Anglais'",
+    'french'=>"Français",
+    'german'=>"Allemand",
+    'english'=>"Anglais",
 ];
