905cb9eb59e293807a514cd089734043a84f4841
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-05-27 09:57:20 +0200

    add orders create logic

--- resources/views/pages/orders/⚡create/create.php
+++ resources/views/pages/orders/⚡create/create.php
@@ -4,11 +4,45 @@ use Livewire\Component;
 
 new class extends Component
 {
+    public  $search = '';
+    public $cart = [];
+    public $searchedProduct = [];
 
     public string $user_id = '';
     public string $project_id = '';
     public string $order_state = '' ;
-    public string $ordered_at ='';
+
+
+    public function updatedSearch()         //ça actualise automatiquement via livewire la search
+    {
+        $this->searchedProduct =  \App\Models\Product::query()
+            ->where('product_name', 'like', '%' . $this->search . '%')
+            ->orWhere('gtin', 'like', '%' . $this->search . '%')
+            ->orWhere('ref_article', 'like', '%' . $this->search . '%')
+            ->orWhere('brand', 'like', '%' . $this->search . '%')
+            ->limit(6)
+            ->get();
+    }
+
+    public function addToOrder( int $productId)
+    {
+    $product = \App\Models\Product::findOrFail($productId);
+
+    if (!isset($this->cart[$productId])){
+        $this->cart[$productId] = [
+            'name' => $product->product_name,
+            'quantity' => 1,
+        ];
+        }
+          $this->search = '';
+        $this->searchedProduct = [];
+    }
+
+
+    public function removeFromOrder( int $productId)
+    {
+        unset($this->cart[$productId]);
+    }
 
     public function store(): void
     {
@@ -16,7 +50,6 @@ new class extends Component
             'user_id'=>'required|integer',
             'order_state'=>'string|required|max:255',
             'project_id'=>'required|integer',
-            'ordered_at'=>'required|date',
         ]);
 
 
@@ -24,8 +57,17 @@ new class extends Component
             'user_id'=>$validated_data['user_id'],
             'order_state'=>$validated_data['order_state'],
             'project_id'=>$validated_data['project_id'],
-            'ordered_at'=>\Carbon\Carbon::now()->format('Y-m-d'),
         ]);
+
+        foreach ($this->cart as $productId=>$item){
+            $order->orderItems()->create([
+               'product_id'=> $productId,
+                'quantity'=> $item['quantity'],
+            ]);
+            \App\Models\Product::findOrFail( $productId)
+                ->decrement('quantity', $item['quantity']);
+        }
+
 
         $this->redirectRoute('pages::orders.index', ['locale' => __('general.currentLocale')]);
     }
