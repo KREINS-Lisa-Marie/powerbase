67f6b5dce29d3d3f070b04a264269239949953d4
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-06-05 15:57:27 +0200

    making order logic

---
+++ resources/views/worker/order/order.php
@@ -0,0 +1,66 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public $cart = [];
+
+    public string $user_id = '';
+    public string $project_id = '';
+    public string $order_state = '' ;
+
+
+    public function mount()
+    {
+        $this->cart = session()->get('cart', []);       //recuperer mon cart
+    }
+
+    public function removeFromOrder( int $productId)
+    {
+        unset($this->cart[$productId]);
+        session()->put('cart', $this->cart);        //je dois faire ça parce que sinon si je recharge, ça n'a pas supprimé le produit
+    }
+
+    public function updatedCart()       //https://livewire.laravel.com/docs/4.x/lifecycle-hooks
+    {
+       session()->put('cart', $this->cart); //je dois faire ça parce que sinon si je recharge, ça n'a pas actualisé la qt
+    }
+
+    public function store(): void
+    {
+        $validated_data= $this->validate([
+            'project_id'=>'required|integer',
+        ]);
+
+        $order = \App\Models\Order::create([
+            'user_id'=> auth()->user()->id,
+            'order_state'=>'pending',
+            'project_id'=>$validated_data['project_id'],
+        ]);
+
+        foreach ($this->cart as $productId=>$item){
+            $order->orderItems()->create([
+                'product_id'=> $productId,
+                'quantity'=> $item['quantity'],
+            ]);
+            \App\Models\Product::findOrFail( $productId)
+                ->decrement('quantity', $item['quantity']);
+        }
+
+        //réinitialiser tout après avoir créé la commande
+        $this->cart=[];
+        session()->put('cart', []);
+        $this->project_id='';
+
+        $this->redirectRoute('worker::order', ['locale' => __('general.currentLocale')]);
+    }
+
+    public function render()
+    {
+        $user = auth()->user()->id;
+        $old_orders = \App\Models\Order::where('user_id', '=', $user)->orderByDesc('id')->get();
+
+        return view('worker::order.order', compact('old_orders', 'user'))->layout('components.worker.app');
+    }
+};
