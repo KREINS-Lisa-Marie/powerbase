2d8ed4224cc89bdb7c854c3cc2358499a3d3fed7
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-06-17 15:46:17 +0200

    most ordered products

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -16,8 +16,16 @@ class HomepageController extends Controller
         $newest_products = Product::orderBy('created_at', 'desc')
             ->limit(4)->get();
 
-        $random_products = Product::inRandomOrder()->limit(8)->get();
+        $random_products = Product::withCount('orderItems')
+            ->orderBy('order_items_count', 'desc')
+            ->limit(8)
+            ->get();
 
         return view('worker.homepage', compact( 'newest_products', 'user', 'random_products'));
     }
 }
+
+/*
+ https://laraveldaily.com/post/eloquent-withcount-get-related-records-amount
+https://laravel.com/docs/13.x/eloquent-relationships#counting-related-models
+*/
