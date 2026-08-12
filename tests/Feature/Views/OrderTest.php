f3fbd4cc09e8ebed710552cf51238dcccb94343c
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-08-12 16:21:14 +0200

    fix route and add routes

--- tests/Feature/Views/OrderTest.php
+++ tests/Feature/Views/OrderTest.php
@@ -22,7 +22,7 @@ it('verifies that the order page is showing it’s contents', function () {
     $locale = app()->getLocale();
     actingAs($user);
 
-    $response = $this->get(route('worker::order', ['locale' => __('general.currentLocale')]));
+    $response = $this->get(route('worker::cart', ['locale' => __('general.currentLocale')]));
 
     $response->assertStatus(200)
         ->assertSeeInOrder(["Commande", "Nom du projet", "Confirmer la commande", "Toutes mes commandes"]);
