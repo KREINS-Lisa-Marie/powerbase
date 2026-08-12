2f2025523725e8a47b1240dcfd3065d7588cd7df
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-08-12 09:01:16 +0200

    add worker orders translations

---
+++ lang/en/worker/orders.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'link_to_order' => 'Order link',
+    'nb_products' => 'Quantity',
+    'ordered_at' => 'Ordered on',
+    'see_the_order' => 'View order',
+    'see_the_product'=>'See the product',
+    'delete_order'=>'Delete order',
+    'link_to_product'=>'Order link',
+    'order_number'=>'Order number',
+    'order_details'=>'Order details',
+];
---
+++ lang/fr/worker/orders.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'link_to_order' => 'Lien commande',
+    'nb_products' => 'Quantité',
+    'ordered_at' => 'Commandé le',
+    'see_the_order' => 'Voir la commande',
+    'see_the_product'=>'Voir le produit',
+    'delete_order'=>'Supprimer la commande',
+    'link_to_product'=>'Lien de commande',
+    'order_number'=>'numéro',
+    'order_details'=>'Détails de la commande',
+];
