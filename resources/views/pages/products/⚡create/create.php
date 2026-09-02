4bc5f0978ad74c4c3475068e49676bd9b28b80ca
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-09-02 20:35:49 +0200

    improve products

--- resources/views/pages/products/⚡create/create.php
+++ resources/views/pages/products/⚡create/create.php
@@ -16,6 +16,7 @@ new class extends Component
     public string $ref_article ='';
     public string $gtin ='';
     public string $quantity = '';
+    public string $comment = '';
     public string $product_description= '';
     public  $product_image = null ;
 
@@ -33,15 +34,18 @@ new class extends Component
             'ref_article'=>'string|required|max:255',
             'gtin'=>['required','string','max:255',Rule::unique('products')],
             'product_description'=>['nullable', 'string'],
+            'comment'=>['nullable', 'string'],
             'quantity'=>'required|int',
             'product_image'=>'image|nullable|mimes:jpg,jpeg,png,webp',
         ]);
 
         if ($this->product_image){
             $image_path = $this->product_image->store(config('productimage.originals_path'), 's3');
+            /*$image_path = $this->product_image->store(config('productimage.originals_path'), 'public');*/
             $filename = basename($image_path); // = juste le nom de l'image sans les dossiers etc
             $image = Image::decode(          //marche pas avec read
                 Storage::disk('s3')->get($image_path)
+                //Storage::disk('public')->get($image_path)
             );
             $sizes = config('productimage.sizes');
             $extension = config('productimage.jpg_image_type');
@@ -57,12 +61,15 @@ new class extends Component
                     $size['height']
                 );
                 \Storage::disk('s3')->put($variant_path.'/'.$filename,
+                                //\Storage::disk('public')->put($variant_path.'/'.$filename,
                 $variant->encodeUsingFormat(\Intervention\Image\Format::JPEG, quality: $compression));
             }
         }
         else{
             $image_path = null;
         }
+
+        $companyId = auth()->user()->company_id;
 
         $product = \App\Models\Product::create([
             'product_name'=>$validated_data['product_name'],
@@ -71,8 +78,16 @@ new class extends Component
             'ref_article'=>$validated_data['ref_article'],
             'gtin'=>$validated_data['gtin'],
             'product_description'=>$validated_data['product_description'],
-            'quantity'=>$validated_data['quantity'],
+            /*'quantity'=>$validated_data['quantity'],*/
             'product_image'=> $image_path,
+            'company_id'=>$companyId
+        ]);
+
+        $productSettings = \App\Models\ProductSetting::create([
+            'company_id'=> $companyId,
+           'product_id'=>$product->id,
+           'quantity'=>$validated_data['quantity'],
+            'comment'=>$validated_data['comment'],
         ]);
 
         $this->redirectRoute('pages::products.index', ['locale' => __('general.currentLocale')]);
