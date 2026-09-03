510b2b960d6ba7d1942e71a5b4ff9beba4951b82
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-09-03 09:43:42 +0200

    added translations

--- resources/views/public/info.blade.php
+++ resources/views/public/info.blade.php
@@ -5,7 +5,7 @@
             POWERBASE
         </h2>
         <p class="medium">
-            L’application gratuite pour la gestion de vos commandes internes
+            {{__('public/info.subtitle')}}
         </p>
     </div>
         <ul class="d-flex flex-wrap flex-r flex-gap-24 public-heading-link-container ">
@@ -16,7 +16,7 @@
                    class="public-heading-link uppercase"
                    title="{{__('navigation.go_boss')}}"
                    aria-label="{{__('navigation.go_boss')}}">
-                    Patrons
+                    {{__('public/info.boss')}}
                 </a>
             </li>
             <li>
@@ -27,7 +27,7 @@
                    class="public-heading-link uppercase"
                    title="{{__('navigation.go_storekeeper')}}"
                    aria-label="{{__('navigation.go_storekeeper')}}">
-                    Magasiniers
+                    {{__('public/info.storekeepers')}}
                 </a>
             </li>
             <li>
@@ -37,7 +37,7 @@
                    class="public-heading-link uppercase"
                    title="{{__('navigation.go_electricians')}}"
                    aria-label="{{__('navigation.go_electricians')}}">
-                    Electriciens
+                    {{__('public/info.electricians')}}
                 </a>
             </li>
         </ul>
@@ -49,16 +49,14 @@
     <div class="m-l-r-main body-content d-flex flex-row flex-wrap flex-gap-24 flex-a-i-center">
     <div class="first-left">
         <h2 class="bold">
-            Découvrez <span class="uppercase colored">Powerbase</span>
+            {{__('public/info.discover')}} <span class="uppercase colored">Powerbase</span>
         </h2>
         <p>
-            Cette application Web permet à aux entreprises d’électricité de centraliser la gestion des produits,
-            commandes, stocks et projets. Elle facilite le travail des électriciens, magasiniers et administrateurs
-            grâce à une interface simple et adaptée aux rôles de chacun.
+            {{__('public/info.description_app')}}
         </p>
     </div>
     <div class="second-right">
-        <img src="{!! asset('assets/img/power.png') !!}" alt="{{__('admin/animals.animal_image')}}" width="600" height="392"
+        <img src="{!! asset('assets/content/power.png') !!}" alt="{{__('public/info.description_image')}}" width="600" height="392"
              class="border-radius-16 public-content-img">
     </div>
     </div>
@@ -67,17 +65,14 @@
     <div class="body-content m-l-r-main">
         <div class="public-top flex-a-i-center">
             <div class="first-left">
-                <img src="{!! asset('assets/img/worker.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
+                <img src="{!! asset('assets/content/worker.jpg') !!}" alt="{{__('public/info.description_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
             </div>
             <div class="second-right">
                 <h2 class="bold">
-                    Espace électricien
+                    {{__('public/info.electricians_interface')}}
                 </h2>
                 <p>
-                    Les électriciens peuvent consulter le catalogue des produits avec des images et des informations
-                    détaillées, rechercher du matériel, ajouter des articles à leur panier et passer des commandes
-                    associées
-                    à un projet. Un système de rappel permet également de limiter les oublis de commande.
+                    {{__('public/info.electricians_description')}}
                 </p>
                 <a href="#storekeeper"
                    lang="{{app()->getLocale()}}"
@@ -85,20 +80,20 @@
                    class="public-heading-link uppercase"
                    title="{{__('public.go_register')}}"
                    aria-label="{{__('public.go_register')}}">
-                    Découvrir l'espace magasinier
+                    {{__('public/info.discover_storekeeper')}}
                 </a>
             </div>
         </div>
         <div class="public-bottom">
             <ul>
                 <li>
-                    <img src="{!! asset('assets/img/b7.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/b7.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
                 <li>
-                    <img src="{!! asset('assets/img/b6.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/b6.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
                 <li>
-                    <img src="{!! asset('assets/img/b8.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/b8.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
             </ul>
         </div>
@@ -109,14 +104,14 @@
     <div class="m-l-r-main body-content d-flex flex-row flex-wrap flex-a-i-center flex-gap-24">
         <div class="first-left">
             <h2 class="bold">
-                Votre atout sur votre concurrence
+                {{__('public/info.your_advantage')}}
             </h2>
             <p>
-                L’application améliore la communication entre les équipes, réduit les erreurs liées aux commandes par téléphone ou papier et permet une meilleure organisation des ressources. Elle offre également une meilleure traçabilité des produits, des projets et des interventions.
+                {{__('public/info.advantage_description')}}
             </p>
         </div>
         <div class="second-right">
-            <img src="{!! asset('assets/img/advantage.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
+            <img src="{!! asset('assets/content/advantage.jpg') !!}" alt="{{__('public/info.description_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
         </div>
         </div>
 </section>
@@ -126,14 +121,14 @@
         <div class="body-content m-l-r-main">
             <div class="public-top flex-a-i-center">
         <div class="first-left">
-            <img src="{!! asset('assets/img/electrician-p.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
+            <img src="{!! asset('assets/content/electrician-p.jpg') !!}" alt="{{__('public/info.description_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
         </div>
         <div class="second-right">
             <h2 class="bold">
-                Espace magasinier
+                {{__('public/info.storekeeper_interface')}}
             </h2>
             <p>
-                Les électriciens peuvent consulter le catalogue des produits avec des images et des informations détaillées, rechercher du matériel, ajouter des articles à leur panier et passer des commandes associées à un projet. Un système de rappel permet également de limiter les oublis de commande.
+                {{__('public/info.storekeeper_description')}}
             </p>
             <a href="#boss"
                lang="{{app()->getLocale()}}"
@@ -141,7 +136,7 @@
                class="public-heading-link uppercase"
                title="{{__('public.go_register')}}"
                aria-label="{{__('public.go_register')}}">
-                Découvrir l'espace patron
+                {{__('public/info.discover_admin')}}
             </a>
         </div>
         </div>
@@ -149,13 +144,13 @@
         <div class="public-bottom">
             <ul>
                 <li>
-                    <img src="{!! asset('assets/img/electrician-d.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/electrician-d.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
                 <li>
-                    <img src="{!! asset('assets/img/electrician-c.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/electrician-c.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
                 <li>
-                    <img src="{!! asset('assets/img/electrician-cc.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/electrician-cc.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
             </ul>
         </div>
@@ -165,12 +160,12 @@
 <section class="public-why body-content m-l-r-main">
     <div class="first-left">
         <h2 class="bold">
-            Pourquoi nous?
+            {{__('public/info.why_us')}}
         </h2>
     </div>
     <div class="second-right">
         <p>
-            L’application améliore la communication entre les équipes, réduit les erreurs liées aux commandes par téléphone ou papier et permet une meilleure organisation des ressources. Elle offre également une meilleure traçabilité des produits, des projets et des interventions.
+            {{__('public/info.why_us_description')}}
         </p>
     </div>
 </section>
@@ -180,7 +175,7 @@
 
 
     <a href="{{route('auth.register', ['locale' => app()->getLocale()])}}" title="{{__('footer.go_to_page_register')}}" class="bold d-flex flex-a-i-center j-c-space-b">
-        Commencez maintenant
+        {{__('public/info.start_now')}}
         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
             <path d="M16 50C16 31.2 31.2 16 50 16C68.8 16 84 31.2 84 50C84 68.8 68.8 84 50 84C31.2 84 16 68.8 16 50ZM80 50C80 33.4 66.6 20 50 20C33.4 20 20 33.4 20 50C20 66.6 33.4 80 50 80C66.6 80 80 66.6 80 50Z" fill="black"/>
             <path d="M46.6008 66.5937L63.2008 49.9937L46.6008 33.3938L49.4008 30.5937L68.8008 49.9937L49.4008 69.3938L46.6008 66.5937Z" fill="black"/>
@@ -197,14 +192,14 @@
         <div class="body-content m-l-r-main">
             <div class="public-top flex-a-i-center">
         <div class="first-left">
-            <img src="{!! asset('assets/img/b3.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
+            <img src="{!! asset('assets/content/b3.jpg') !!}" alt="{{__('public/info.description_image')}}" width="600" height="392" class="border-radius-16 public-content-img">
         </div>
         <div class="second-right">
             <h2>
-                Espace patron
+                {{__('public/info.admin_interface')}}
             </h2>
             <p>
-                Les électriciens peuvent consulter le catalogue des produits avec des images et des informations détaillées, rechercher du matériel, ajouter des articles à leur panier et passer des commandes associées à un projet. Un système de rappel permet également de limiter les oublis de commande.
+                {{__('public/info.admin_description')}}
             </p>
             <a href="{{route('auth.register', ['locale' => app()->getLocale()])}}"
                lang="{{app()->getLocale()}}"
@@ -212,20 +207,20 @@
                class="public-heading-link uppercase"
                title="{{__('public.go_register')}}"
                aria-label="{{__('public.go_register')}}">
-                Créer un compte
+                {{__('public/info.create_account')}}
             </a>
         </div>
         </div>
         <div class="public-bottom">
             <ul>
                 <li>
-                    <img src="{!! asset('assets/img/b4.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/b4.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
                 <li>
-                    <img src="{!! asset('assets/img/b5.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/b5.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
                 <li>
-                    <img src="{!! asset('assets/img/b1.jpg') !!}" alt="{{__('admin/animals.animal_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
+                    <img src="{!! asset('assets/content/b1.jpg') !!}" alt="{{__('public/info.description_image')}}" width="392" height="288" class="border-radius-16 public-content-img">
                 </li>
             </ul>
         </div>
@@ -236,8 +231,8 @@
 
         <div class="m-l-r-main body-content">
             <a href="{{route('public.contact', ['locale' => app()->getLocale()])}}" title="{{__('footer.go_to_page_contact')}}" class="bold">
-                Besoin de plus d’informations?
-                Contactez-nous!
+                {{__('public/info.more_info')}}
+                {{__('public/info.contact_us_now')}}
             </a>
             <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
                 <path
